9ce951b5e9ca6091c59b024be77a37fd09441284
Paul Emanuel Wiese <ge87cop@mytum.de>
2025-01-18 12:19:58 +0100

    support update

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "f62a899ca6079120662df079a75eaede",
+const RESOURCES = {"flutter_bootstrap.js": "c31a0226a1ccda73ff2ecc7315d23b25",
 "version.json": "e0fd5e466aba859dc780719e13ad0811",
 "index.html": "35cbef5cd4bbf5a17588c55e5555d503",
 "/": "35cbef5cd4bbf5a17588c55e5555d503",
-"main.dart.js": "b188fb47ce24f753280c669da2180b6f",
+"main.dart.js": "5525d70af463e0a5e06c10de435b48f2",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
--- lib/main.dart
+++ lib/main.dart
@@ -6,24 +6,21 @@ import 'pages/index.dart';
 
 import 'models/medication.dart';
 
-
 void main() async {
-
   await Hive.initFlutter();
 
   Hive.registerAdapter(MedicationAdapter());
 
   await Hive.openBox('medication');
-  
+
   await handleDateTimeField();
 
   runApp(const MainApp());
-
 }
 
 Future<void> handleDateTimeField() async {
   final box = Hive.box('medication');
-  final lastSupportedDate = DateTime(2024, 01, 17);
+  final lastSupportedDate = DateTime(2024, 01, 18);
 
   try {
     final lastUpdated = box.get(13) as DateTime?;
@@ -55,37 +52,33 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
-  
   var box = Hive.box('medication');
-  
+
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        textTheme: const TextTheme(
-          bodyLarge: TextStyle(fontSize: 14),
-          bodyMedium: TextStyle(fontSize: 12),
-          bodySmall: TextStyle(fontSize: 10),
-          titleLarge: TextStyle(fontSize: 18),
-          titleMedium: TextStyle(fontSize: 16),
-          titleSmall: TextStyle(fontSize: 14),
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          textTheme: const TextTheme(
+            bodyLarge: TextStyle(fontSize: 14),
+            bodyMedium: TextStyle(fontSize: 12),
+            bodySmall: TextStyle(fontSize: 10),
+            titleLarge: TextStyle(fontSize: 18),
+            titleMedium: TextStyle(fontSize: 16),
+            titleSmall: TextStyle(fontSize: 14),
+          ),
+          colorScheme: const ColorScheme(
+            brightness: Brightness.light,
+            primary: Color.fromARGB(255, 11, 62, 140),
+            onPrimary: Colors.white,
+            secondary: Colors.black,
+            onSecondary: Colors.black,
+            error: Colors.red,
+            onError: Colors.white,
+            surface: Color.fromARGB(255, 250, 250, 252),
+            onSurface: Colors.black,
+          ),
         ),
-        colorScheme: const ColorScheme(
-          brightness: Brightness.light,
-          primary: Color.fromARGB(255, 11, 62, 140),
-          onPrimary: Colors.white,
-          secondary: Colors.black,
-          onSecondary: Colors.black,
-          error: Colors.red,
-          onError: Colors.white,
-          surface: Color.fromARGB(255, 250, 250, 252),
-          onSurface: Colors.black,
-        ),
-        
-      ),
-      home: box.get(1) == 'ready' ? const Navigation() : const Start()
-    );
+        home: box.get(1) == 'ready' ? const Navigation() : const Start());
   }
 }
